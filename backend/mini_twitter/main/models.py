fb302931b041d28459a1ac842b510024e8f74cc0
gauravdaunde <gauravdaunde@gmail.com>
2020-09-03 20:36:48 +0530

    changes after code review

--- backend/mini_twitter/main/models.py
+++ backend/mini_twitter/main/models.py
@@ -2,16 +2,26 @@ import os
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+
+
 
 
 #function to rename and return path path to store profile image
-def image_path_rename(profile_instance, image_filename):
+def image_path_rename(profile_obj, image_filename):
     upload_to = 'profile_images'
     image_extension = image_filename.split('.')[-1]
 
     #renaming image with user's username.extension
-    image_name = '{}.{}'.format(profile_instance.user.username, image_extension)
+    image_name = '{}.{}'.format(profile_obj.user.username, image_extension)
     return os.path.join(upload_to, image_name)
+
+
+
+@receiver(models.signals.post_save, sender=User)
+def user_created(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
 
 
 class UserProfile(models.Model):
@@ -21,6 +31,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.ImageField(upload_to=image_path_rename,null=True)
     bio = models.CharField(max_length=255)
+
+
 
 
 class Tweet(models.Model):
@@ -44,3 +56,5 @@ class Follow(models.Model):
 
     class Meta:
         unique_together = (('follower','following'))
+
+
--- backend/mini_twitter/main/serializers.py
+++ backend/mini_twitter/main/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 from rest_framework import serializers
 
@@ -8,39 +9,47 @@ from .models import (UserProfile,
                      Follow)
 
 
-
 class UserSerializer(serializers.ModelSerializer):
+    first_name= serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    follow = serializers.SerializerMethodField()
+
+    #if logged in user follows current serializer user instance then follow field is true otherwise false
+    def get_follow(self, user_obj):
+        user = self.context['request'].user
+        return (user.id,) in user_obj.following_set.all().values_list('follower__id')
+
+    #override for password hashing
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(UserSerializer, self).create(validated_data)
+
 
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'username', 'password']
+        fields = ['id', 'first_name', 'last_name', 'username', 'password','follow']
+        extra_kwargs = {'password': {'write_only': True}}
 
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     following = serializers.SerializerMethodField()
     followers = serializers.SerializerMethodField()
 
-    full_name = serializers.SerializerMethodField()
-    username = serializers.SerializerMethodField()
-
-    def get_full_name(self, profile_obj):
-        return (profile_obj.user.first_name + ' ' + profile_obj.user.last_name)
-
-    def get_username(self, profile_obj):
-        return profile_obj.user.username
 
     def get_followers(self, profile_obj):
-        return profile_obj.user.follower_set.all().values('following__id', 'following__first_name',
-                                                                  'following__last_name', 'following__username')
+        return profile_obj.user.following_set.all().values('id','follower__id', 'follower__first_name',
+                                                                    'follower__last_name', 'follower__username')
 
     def get_following(self, profile_obj):
-        return profile_obj.user.following_set.all().values('follower__id', 'follower__first_name',
-                                                                    'follower__last_name', 'follower__username')
+        return profile_obj.user.follower_set.all().values('id', 'following__id', 'following__first_name',
+                                                                  'following__last_name', 'following__username')
+
 
     class Meta:
         model = UserProfile
-        fields = ['id','username','full_name', 'bio','profile_image','followers','following']
+        fields = ['user', 'bio','profile_image','followers','following']
 
 
 class FollowSerializer(serializers.ModelSerializer):
@@ -54,18 +63,13 @@ class FollowSerializer(serializers.ModelSerializer):
 
 
 class TweetSerializer(serializers.ModelSerializer):
-    full_name = serializers.SerializerMethodField()
-    username = serializers.SerializerMethodField()
-
-    def get_full_name(self,tweet_obj):
-        return (tweet_obj.user.first_name + ' ' + tweet_obj.user.last_name)
-
-    def get_username(self, tweet_obj):
-        return tweet_obj.user.username
-
+    user_id = serializers.CharField(source='user.id', required=False)
+    username = serializers.CharField(source='user.username', required=False)
+    first_name = serializers.CharField(source='user.first_name', required=False)
+    last_name = serializers.CharField(source='user.last_name', required=False)
 
     class Meta:
         model = Tweet
-        fields = ['id','full_name','username', 'content', 'created_on']
+        fields = ['user_id','first_name','last_name','username', 'content', 'created_on']
 
 
